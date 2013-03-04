448264796400be4a92af2cc4baeea2798357b03e
Michal J. Gajda <mjgajda@gmail.com>
2013-03-04 10:49:19 +0100

    Added script that prints names and scores for N best decoys.

---
+++ scripts/selectBest.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables, OverloadedStrings #-}
+module Main where
+
+import System.IO
+import System.Environment
+import qualified Data.ByteString.Char8 as BS
+import Data.List(minimumBy)
+import Control.Monad(forM)
+
+import Rosetta.Silent
+
+--   TODO: optional trailing arguments - extract only given decoys
+main = do [number, silentInputFilename, pdbOutputFilename] <- getArgs
+          let ((n :: Int, []):_) = reads number
+          mdls <- processSilent $ BS.pack silentInputFilename
+          let bestMdls = take n $ sortModelsByScore mdls
+          forM bestMdls $ \bestMdl ->
+            putStrLn $ BS.unpack (name bestMdl) ++ ": " ++ show (modelScoreIfAvailable bestMdl)
+          -- TODO: write silent output
+
+
+
