d0325db24001ff0153c0038c53b5c38ab306e927
Michal J. Gajda <mjgajda@gmail.com>
2013-03-05 13:08:34 +0100

    Write silent file in selectBest.

--- scripts/selectBest.hs
+++ scripts/selectBest.hs
@@ -10,13 +10,13 @@ import Control.Monad(forM)
 import Rosetta.Silent
 
 --   TODO: optional trailing arguments - extract only given decoys
-main = do [number, silentInputFilename, pdbOutputFilename] <- getArgs
+main = do [number, silentInputFilename, silentOutputFilename] <- getArgs
           let ((n :: Int, []):_) = reads number
           mdls <- processSilentFile $ BS.pack silentInputFilename
           let bestMdls = take n $ sortModelsByScore mdls
           forM bestMdls $ \bestMdl ->
             putStrLn $ BS.unpack (name bestMdl) ++ ": " ++ show (modelScoreIfAvailable bestMdl)
-          -- TODO: write silent output
+          writeSilentFile silentOutputFilename bestMdls
 
 
 
