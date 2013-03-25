263f2df4798811813e5f5d4c8a2c1c37cc3764ee
Michal J. Gajda <mjgajda@gmail.com>
2013-03-25 20:57:04 +0100

    Corrected Restraints.Bounded.bEst -> bStDev.

--- Rosetta/Restraints.hs
+++ Rosetta/Restraints.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings, DeriveDataTypeable, PatternGuards #-}
 -- | Module for parsing and processing ROSETTA 3.x restraints.
-module Rosetta.Restraints( Restraint(..)
-                         , AtomId   (..)
+module Rosetta.Restraints( Restraint        (..)
+                         , RestraintFunction(..)
+                         , AtomId           (..)
                          , parseRestraints
                          , parseRestraintsFile
                          , processRestraintsFile
@@ -23,19 +24,29 @@ import Rosetta.Util
 -- TODO: Use unique strings for atom and residue codes.
 -- Packages: stringtable-atom, monad-atom, simple-atom
 
--- | Description of ROSETTA 3.x restraint type.
-data Restraint = DistR { at1, at2 :: !AtomId ,
-                         goal     :: !Double }
-               | DiheR { at1, at2, at3, at4 :: !AtomId ,
-                         goal               :: !Double }
+-- | Scoring function for a restraint.
+data RestraintFunction = RGaussian { gAvg,     gStDev,   gGoal  :: !Double }
+                       | RBounded  { bLoBound, bHiBound, bStDev :: !Double }
   deriving (Read, Eq, Data, Typeable)
+
+-- | Description of ROSETTA 3.x restraint type.
+data Restraint = DistR { at1, at2 :: !AtomId
+                       , func     :: !RestraintFunction }
+               | DiheR { at1, at2, at3, at4 :: !AtomId
+                       , func               :: !RestraintFunction }
+  deriving (Read, Eq, Data, Typeable)
+
 -- TODO: define Show that prints out Rosetta format?
 
+instance Show RestraintFunction where
+  showsPrec n (RGaussian avg dev goal) = ("GAUSSIAN "++) . showsPrec n avg . (' ':) . showsPrec n dev . (' ':) . showsPrec n goal
+  showsPrec n (RBounded  lo  hi  est ) = ("BOUNDED "++)  . showsPrec n lo  . (' ':) . showsPrec n hi  . (' ':) . showsPrec n est
+
 instance Show Restraint where
-  showsPrec n (DistR at1 at2 goal) s = ("AtomPair "++) . showsAt at1 . (" GAUSSIAN "++) . showsPrec n goal . (" 1.00"++) $ s
-    where
-      showsAt :: AtomId -> String -> String
-      showsAt at s = showsPrec n (resId at) $ " " ++ BS.unpack (resName at) ++ " " ++ BS.unpack (atName at) ++ s
+  showsPrec n (DistR at1 at2 func) = ("AtomPair "++) . showsAt n at1 . (' ':) . showsAt n at2 . showsPrec n func
+
+showsAt :: Int -> AtomId -> String -> String
+showsAt n at s = showsPrec n (resId at) $ " " ++ BS.unpack (resName at) ++ " " ++ BS.unpack (atName at) ++ s
 
 -- | Datatype pointing to a given atom in a molecule.
 data AtomId = AtomId { resName :: !BS.ByteString, -- may be empty!
@@ -50,12 +61,18 @@ instance NFData AtomId    where
 instance NFData Restraint where
 
 -- | Parse constraint function header and parameters
+parseFunc ::  Show a => a -> [BS.ByteString] -> Either [Char] RestraintFunction
 parseFunc lineNo (funcs:spec) | funcs == "GAUSSIAN" =
                                   do when (length spec <  2) $ Left $ "Not enough arguments to GAUSSIAN in line " ++ show lineNo
-                                     parseFloat lineNo $ spec !! 0
+                                     [avg, dev] <- mapM (\i -> parseFloat lineNo $ spec !! i) [0, 1]
+                                     est <- if length spec == 2
+                                              then return avg
+                                              else parseFloat lineNo $ spec !! 2
+                                     return $! RGaussian avg dev est
 parseFunc lineNo (funcs:spec) | funcs == "BOUNDED"  = 
                                   do when (length spec <= 3) $ Left $ "Not enough arguments to BOUNDED in line "  ++ show lineNo
-                                     parseFloat lineNo $ spec !! 1
+                                     [hi, lo, stdev] <- mapM (\i -> parseFloat lineNo $ spec !! i) [0..2]
+                                     return $! RBounded { bLoBound = lo, bHiBound = hi, bStDev = stdev }
 parseFunc lineNo (funcs:spec)                       = 
                                      Left $ "Unknown function name " ++ show (BS.unpack funcs)
 
@@ -67,8 +84,8 @@ parseFloat lineNo floatStr                                           =
 -- | Parse an distance restraint (AtomPair)
 parsePair lineNo ws = do when (len <= 8) $ Left $ "Too few ("++ show len ++") words in AtomPair line!"
                          [at1, at2] <- mapM (mkAtId3 lineNo) [at1s, at2s]
-                         goal <- parseFunc lineNo funcs
-                         Right $ DistR at1 at2 goal
+                         func <- parseFunc lineNo funcs
+                         Right $ DistR at1 at2 func
   where
     len = length ws
     [at1s, at2s, funcs] = splitsAt [3, 6] ws
